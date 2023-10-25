a5378eb57ab641c01a21387c02a9d2d7c8637898
Chrapson <marcin.chraplewski@gmail.com>
2023-10-26 00:50:35 +0200

    [chore] options styling sketch

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Taboo from "./components/Taboo/Taboo";
 import TabooOptions from "./components/TabooOptions/TabooOptions";
-import Translator from "./components/Translator/Translator";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/random-word" element={<Translator />} />
         <Route path="/" element={<TabooOptions />} />
         <Route path="/game" element={<Taboo />} />
       </Routes>
